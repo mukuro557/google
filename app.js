115a06b05c975593f6f8e95b298501d12a8abeb3
mukuro557 <56430402+mukuro557@users.noreply.github.com>
2020-03-13 15:49:37 +0700

    logout button

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use("/profile", profileRoutes);
 
 app.get("/", (req, res) => {
     // res.sendFile(path.join(__dirname,"views/home.html"));
-    res.render("home.ejs")
+    res.render("home.ejs" ,{user : req.user})
 
 });
 
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("passport");
 
 router.get("/login", (req, res) => {
-    res.render("login.ejs");
+    res.render("login.ejs",{user : req.user});
 });
 //Login google
 router.get("/google",passport.authenticate("google",{scope: ["profile", "email"]}));
@@ -11,6 +11,12 @@ router.get("/google/redirect",passport.authenticate("google"), (req, res) => {
 // res.send("Login OK, show profile");
 // res.send(req.user);
 res.redirect("/profile")
+});
+
+//logout
+router.get("/logout", (req, res) => {
+    req.logOut();
+    res.redirect("/");
 });
 
 module.exports = router;
